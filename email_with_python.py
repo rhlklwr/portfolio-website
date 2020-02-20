e45879aa364a622f6385daeb335131dd05e57fda
rhlklwr <kalwar.rahul@gmail.com>
2020-02-20 20:08:52 +0530

    My new website

---
+++ email_with_python.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def email_body(data):
+    email = EmailMessage()
+    email['from'] = 'Rahul Kalwar'
+    email['to'] = data
+    email['subject'] = 'Resume'
+    email.set_content(
+        "Hello Dear\n\n"
+        "Please find my resume in attached document.\n\n"
+        "Regards,\n"
+        "Rahul Kalwar")
+    with open('./static/resume.pdf', 'rb') as content_file:
+        content = content_file.read()
+        email.add_attachment(content, maintype='application/pdf', subtype='pdf', filename='resume.pdf')
+    return email
+
+
+def email_sent(email_object_to_sent):
+    with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login("emailwithpython8@gmail.com", 'ckyolvyreghabjoa')
+        smtp.send_message(email_object_to_sent)
